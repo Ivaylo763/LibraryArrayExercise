ac8fd669181b9d2fda9ce1f86e0c641dc522f97c
Ivaylo763 <iedzhurelov24a@gmail.com>
2026-03-10 14:03:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if st.button("Добави книгата"):
   st.success("Книгата е добавена!")
 
 if st.button("Покажи всички книги"):
-if len(se.session_state.books) == 0:
+if len(st.session_state.books) == 0:
 st.write("Няма добавени книги")
 else:
   for book in st.session_state.books:
